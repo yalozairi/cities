c2fa72934bb3f98ae9a9e9a1db6f07806590f895
Yousif AlOzairi <yoyo012@icloud.com>
2020-07-09 18:01:58 +0300

    first commit

---
+++ src/cities.js
@@ -0,0 +1,33 @@
+
+
+const cities = [
+    {
+    name:
+    main image:
+    description:
+    google maps:
+    climate:
+    time zone:
+    attractions {
+    1 {
+    name:
+     image:
+    description:
+    target tourists: }
+    2{
+    name:
+     image:
+    description:
+    target tourists: }
+    3{
+    name:
+     image:
+    description:
+    target tourists: }
+    }
+    recommendations:
+    },
+
+]
+
+export default cities;
